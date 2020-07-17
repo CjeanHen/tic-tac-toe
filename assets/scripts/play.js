a1c52229f7a323d0b5191f4588f38ae83d04fb6d
CjeanHenn <carolynhennessey95@gmail.com>
2020-07-17 16:23:24 +0200

    return object with cells and over

--- assets/scripts/play.js
+++ assets/scripts/play.js
@@ -11,67 +11,23 @@ const onTakeTurn = event => {
       $(cellPlayed).html('<h1>X</h1>')
       cells[cellIndex] = 'x'
       cellPlayed.setAttribute('data-isOpen', 'no')
-      console.log(cells)
       isOver(cells)
-      console.log(over)
       turn++
     } else {
       $(cellPlayed).html('<h1>O</h1>')
       cells[cellIndex] = 'o'
       cellPlayed.setAttribute('data-isOpen', 'no')
-      console.log(cells)
       isOver(cells)
-      console.log(over)
       turn++
     }
   }
+  const game = {
+    cells: cells,
+    over: over
+  }
+  console.log(game)
+  return game
 }
-
-// WAYS TO WIN:
-// cells[0] === cells[1] && cells[1] === cells[2]
-// cells[3] === cells[4] && cells[4] === cells[5]
-// cells[6] === cells[7] && cells[7] === cells[8]
-// cells[0] === cells[3] && cells[3] === cells[6]
-// cells[1] === cells[4] && cells[4] === cells[7]
-// cells[2] === cells[5] && cells[5] === cells[8]
-// cells[0] === cells[4] && cells[4] === cells[8]
-// cells[2] === cells[4] && cells[4] === cells[3]
-
-// const isOver = cells => {
-//   if (cells[0] === cells[1] && cells[1] === cells[2]) {
-//     if (cells[0] === 'x' || cells[0] === 'o') {
-//       over = true
-//     }
-//   } else if (cells[3] === cells[4] && cells[4] === cells[5]) {
-//     if (cells[3] === 'x' || cells[3] === 'o') {
-//       over = true
-//     }
-//   } else if (cells[6] === cells[7] && cells[7] === cells[8]) {
-//     if (cells[6] === 'x' || cells[6] === 'o') {
-//       over = true
-//     }
-//   } else if (cells[0] === cells[3] && cells[3] === cells[6]) {
-//     if (cells[0] === 'x' || cells[0] === 'o') {
-//       over = true
-//     }
-//   } else if (cells[1] === cells[4] && cells[4] === cells[7]) {
-//     if (cells[1] === 'x' || cells[1] === 'o') {
-//       over = true
-//     }
-//   } else if (cells[2] === cells[5] && cells[5] === cells[8]) {
-//     if (cells[2] === 'x' || cells[2] === 'o') {
-//       over = true
-//     }
-//   } else if (cells[0] === cells[4] && cells[4] === cells[8]) {
-//     if (cells[0] === 'x' || cells[0] === 'o') {
-//       over = true
-//     }
-//   } else if (cells[2] === cells[4] && cells[4] === cells[3]) {
-//     if (cells[2] === 'x' || cells[2] === 'o') {
-//       over = true
-//     }
-//   }
-// }
 
 const isOver = cells => {
   if ((cells[0] === cells[1] && cells[1] === cells[2]) && (cells[0] === 'x' || cells[0] === 'o')) {
